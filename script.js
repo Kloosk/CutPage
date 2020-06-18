40595e0b397ce5123fa51b0afef0ebc129f84b9a
Lukasz <lukasz.gras.97@gmail.com>
2020-06-19 00:12:41 +0200

    Added cut for all elements

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+// disintegrate.init();
 let cut = document.querySelector(".cut");
 let menu = document.querySelector(".menu");
 let inp = document.querySelector(".inp__txt");
 let main = document.querySelector("main");
 let win = document.querySelector(".win");
+let cutTxt = document.querySelector(".cutTxt");
+let header = document.querySelector("header");
+let char2 = document.querySelector(".char2");
+let nav = document.querySelector("nav");
+let bodyRotate = document.querySelector("body");
+
 let animeStart = false;
+let currentCut = 0;
 let eve;
 const anime = () => {
    win.style.transitionDuration = "0s";
@@ -17,7 +25,8 @@ const anime = () => {
 
 };
 const hacker = () => {
-    if(inp.value === "ggwp" || inp.value === "GGWP"){
+    let inpVal = inp.value.toLowerCase();
+    if(inpVal === "ggwp"){
         main.style.transform = `translate(-50%,130vh)`;
         setTimeout(() => {
             main.style.transform = `translate(-50%,0)`;
@@ -28,8 +37,22 @@ const hacker = () => {
         },2500);
     }
 };
-
-
+nav.addEventListener("click",() => {
+   bodyRotate.style.transform = `translateY(100vh) rotate(180deg) `;
+},{once:true});
+char2.addEventListener("click",() => {
+    char2.style.transform =  `translateX(-150vw) rotate(360deg)`;
+}, {once : true});
+header.addEventListener("click",() => {
+   header.style.transform = "translateX(-100vw)";
+   setTimeout(() => {
+       header.style.transform = "translateX(100vw)";
+       setTimeout(() => {
+           char2.style.transform =  `translateX(-150vw) rotate(-90deg)`;
+           header.style.transform = "translateX(-50%)";
+       },1000);
+   },1000);
+}, {once : true});
 document.addEventListener("contextmenu",e => {
     e.preventDefault();
     console.log(e);
@@ -62,7 +85,11 @@ inp.addEventListener("keyup",() => {
 });
 cut.addEventListener("click",() => {
     console.log(eve.target.parentElement);
+    // const disObj = disintegrate.getDisObj(eve.target);
+    // disintegrate.createSimultaneousParticles(disObj);
     eve.target.parentElement.remove();
+    currentCut++;
+    cutTxt.innerText = `Current cut: ${currentCut}`;
 });
 window.addEventListener("click",() => {
     menu.style.visibility = "hidden";
