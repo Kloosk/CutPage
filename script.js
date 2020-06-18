8c091b803df2a5856534aba161361806d8790e09
Lukasz <lukasz.gras.97@gmail.com>
2020-06-18 21:32:47 +0200

    Added finish function

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 let cut = document.querySelector(".cut");
 let menu = document.querySelector(".menu");
 let inp = document.querySelector(".inp__txt");
+let main = document.querySelector("main");
+let win = document.querySelector(".win");
+let animeStart = false;
 let eve;
+const anime = () => {
+   win.style.transitionDuration = "0s";
+   win.style.transitionDelay = "0s";
+    setInterval(() => {
+        console.log("work");
+        animeStart = !animeStart;
+        animeStart ? win.style.transform = `translate(0vw,0vh)` : win.style.transform = `translate(-0.5vw,-0.5vh)`;
+    },500)
 
+
+};
 const hacker = () => {
-
+    if(inp.value === "ggwp" || inp.value === "GGWP"){
+        main.style.transform = `translate(-50%,130vh)`;
+        setTimeout(() => {
+            main.style.transform = `translate(-50%,0)`;
+            win.style.transform = `translate(-1vw,-1vh)`;
+            setTimeout(() => {
+                anime();
+            },2500)
+        },2500);
+    }
 };
 
 
@@ -27,7 +49,7 @@ inp.addEventListener("keydown",e => {
         inp.style.transform = `rotate(${rotateInp}deg)`;
     }
 });
-inp.addEventListener("keypress",() => {
+inp.addEventListener("keyup",() => {
     if(rotateInp < 90) {
         paddingInp += 10;
         rotateInp += 20;
